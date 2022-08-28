16b080bd25cba1f802b66aa8941906acd9671ed2
Kerwindows <kerwindows@hotmail.com>
2022-08-28 09:50:19 -0400

    first commit

---
+++ src/serverData.js
@@ -0,0 +1,46 @@
+const serverData = {
+  "friends": [
+    {
+      "name": "Bill",
+			"id": "0",
+      "profilePicDark":"/profile-images/bill-dark.png",
+      "profilePicLight":"/profile-images/bill-light.png",
+			"location": "Boise, Idaho",
+			"favBirdQuote": "The early bird gets the worm",
+      "parrotsOwned": [
+        {
+          "name": "Jimjim",
+          "favoriteToys": [
+            "ball",
+            "ribbon"
+          ]
+        },
+        {
+          "name": "Charles",
+          "favoriteToys": [
+            "the little cup",
+            "his bear"
+          ]
+        }
+      ]
+    },
+    {
+      "name": "Mary",
+			"id": "1",
+      "profilePicDark":"/profile-images/mary-dark.png",
+      "profilePicLight":"/profile-images/mary-light.png",
+			"location": "London, England",
+			"favBirdQuote": "A bird in the hand is worth two in the bush",
+      "parrotsOwned": [
+        {
+          "name": "Smitty",
+          "favoriteToys": [
+            "pencil",
+            "squeaker"
+          ]
+        }
+      ]
+    }
+  ]
+}
+export default serverData;
